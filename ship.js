d504263056d945c24b7aba600a802ac8539a77c0
Jack <jackrileynicosia@yahoo.com>
2024-02-21 15:32:59 -0500

    Ship and gameboard working. Player is WIP

---
+++ ship.js
@@ -0,0 +1,16 @@
+class Ship {
+  constructor(size) {
+    this.size = size;
+    this.hits = 0;
+  }
+
+  hit() {
+    this.hits++;
+  }
+
+  isSunk() {
+    return this.hits >= this.size;
+  }
+}
+
+module.exports = { Ship };
